4baf9a6e70dfb9fe3cd2391c5b2903393c761dc2
dkdkwl <dkdkwl@naver.com>
2022-06-11 18:01:33 +0900

    리액트 state만들기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import{useState} from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['서울 우동 맛집','인천 우동 맛집','부천 우동 맛집']);
+  let [logo, setLogo] = useState('ReactBlog');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{logo}</h4>
+      </div>
+      <div className="List">
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="List">
+        <h4>{글제목[1]}</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className="List">
+        <h4>{글제목[2]}</h4>
+        <p>2월 19일 발행</p>
+      </div>
+      <h4 style={{color:'red',fontSize:'20px'}}>{post}</h4>
     </div>
   );
 }
