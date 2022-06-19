c2642911ee8b5f14f61139f26a33a13080065857
dkdkwl <dkdkwl@naver.com>
2022-06-19 14:05:20 +0900

    status 모달 변경하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,49 +7,19 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','부천 우동 맛집']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal,setModal] = useState(false);
-
+  let [title,setTitle] = useState(0);
 
   return (
     <div className="App">
-      <div className="black-nav">
-        <h4></h4>
-      </div>
-      <button onClick={()=>{
-        let copy = [...글제목];
-        copy[0] = '여자코트 추천';
-        글제목변경(copy)
-      }}>글 수정</button>
-
-      <button onClick={
-        ()=>{
-          let titleSort = [...글제목];
-          titleSort.sort();
-          글제목변경(titleSort)
-        }
-      }>순서변경하기</button>
-
-      <div className="List">
-        <h4>{글제목[0]} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="List">
-        <h4>{글제목[1]}111</h4>
-        <p>2월 18일 발행</p>
-      </div>
-      <div className="List">
-        <button onClick = {()=>{setModal(!modal)}}>{글제목[2]}팝업버튼</button>
-        <p>2월 19일 발행</p>
-      </div>
       <h4 style={{color:'red',fontSize:'20px'}}>{post}</h4>
-      {
-        modal == true ? <Modal/> : null
-      }
-
       {
         글제목.map(function(a,i){
           return (
           <div className="List" key = {i}>
-            <h4>
+            <h4 onClick={()=>{
+              setModal(true);
+              setTitle(i);
+            }}>{글제목[i]}
             <span onClick={() => {
               let copy = [...따봉];
               copy[i] = copy[i]+1;
@@ -61,23 +31,29 @@ function App() {
           )
         })
       }
-      
+
+      <button onClick={()=>{setTitle(0)}}>글제목0</button>
+      <button onClick={()=>{setTitle(1)}}>글제목1</button>
+      <button onClick={()=>{setTitle(2)}}>글제목2</button>
+
+        
+      {
+        modal == true ? <Modal title = {title} 글제목변경={글제목변경} color={'coral'} 글제목={글제목}/> : null
+      }
     </div>
-
-
   );
 }
 
 
-function Modal(){
+
+function Modal(props){
   return (
     <>
       <div className='modal'>
-        <h4>제목</h4>
+        <h4>{props.글제목[props.title]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
-      <div></div>
     </>
   )
 }
